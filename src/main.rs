4d6d66382698b34fcd3e1e1a5c9afa6db1b6dfa1
Askepsa <jam.lugtus@gmail.com>
2024-11-15 10:05:11 +0800

    the plan

--- src/main.rs
+++ src/main.rs
@@ -1,130 +1,85 @@
 // #![allow(unused)]
 
-use opencv::{
-    core::{
-        self, bitwise_and, in_range, no_array, type_to_str, Mat, MatExprTraitConst, MatTraitConst,
-        Point, Scalar, CV_8UC3,
-    },
-    highgui::{imshow, set_mouse_callback, wait_key, MouseEventTypes},
-    imgcodecs::{imread, IMREAD_COLOR},
-    imgproc::{
-        canny, circle_def, cvt_color, draw_contours, find_contours, moments, CHAIN_APPROX_SIMPLE,
-        COLOR_BGRA2GRAY, LINE_8, RETR_EXTERNAL,
-    },
-    Result,
-};
+use opencv::core::{no_array, Mat, Point, Scalar, Vector};
+use opencv::highgui::{imshow, set_mouse_callback, wait_key_def, MouseEventTypes};
+use opencv::imgcodecs::{self, imread};
+use opencv::imgproc::{self, adaptive_threshold, draw_contours, find_contours};
+use opencv::Result;
 
-fn print_coords(event: i32, x: i32, y: i32, _: i32) {
+use kalosm_ocr::Ocr;
+
+// view img
+fn main() -> Result<()> {
+    let mut model = Ocr::builder().build();
+
+    Ok(())
+}
+
+fn _do_something() -> Result<()> {
+    let img = imread("./monke.png", imgcodecs::IMREAD_GRAYSCALE)?;
+
+    let mut edge = Mat::default();
+    // canny_def(&img, &mut edge, 200., 255.)?;
+    adaptive_threshold(
+        &img,
+        &mut edge,
+        128.,
+        imgproc::ADAPTIVE_THRESH_MEAN_C,
+        imgproc::THRESH_BINARY_INV,
+        7,
+        3.,
+    )?;
+
+    // get coords
+    let mut contours: Vector<Vector<Point>> = Vector::default();
+    find_contours(
+        &edge,
+        &mut contours,
+        imgproc::RETR_EXTERNAL,
+        imgproc::CHAIN_APPROX_SIMPLE,
+        Point::new(0, 0),
+    )?;
+
+    // make struct of img (uuid: String, contour, val: Option<u32>)
+    // crop images and then save to array (with uuid) and file
+    // make tesseract read each images and add corresponding uuid
+    // as key to hashmap and its corresponding parsed number as value
+
+    for contour in contours.iter() {
+        println!("{:#?}\n\n\n", contour);
+    }
+
+    // get store values of hashmap to an array and sort it
+    // for each img (struct) then get centroid coords and
+    // make rustautogui move the mouse and click the
+    // coords
+
+    let mut img = imread("./monke.png", imgcodecs::IMREAD_COLOR)?;
+    draw_contours(
+        &mut img,
+        &contours,
+        -1,
+        Scalar::new(0., 0., 255., 0.),
+        2,
+        imgproc::LINE_8,
+        &no_array(),
+        1,
+        Point::new(0, 0),
+    )?;
+
+    while wait_key_def()? != 'q' as i32 {
+        set_mouse_callback("Monke", Some(Box::new(_print_coords)))?;
+        imshow("Monke", &img)?;
+    }
+
+    Ok(())
+}
+
+fn _print_coords(event: i32, x: i32, y: i32, _: i32) {
     if let Ok(event) = MouseEventTypes::try_from(event) {
         match event {
             MouseEventTypes::EVENT_LBUTTONDOWN => println!("x: {x} y: {y}"),
             _ => (),
         }
     };
-}
-
-// bilangin kung ilang green rectangle ang meron
-// at lagyan ng label sa original image
-// at lagyan ng label ang sentro ng object
-fn main() -> Result<()> {
-    // read image
-    let mut img = imread("./photo.png", IMREAD_COLOR)?; // Mat uchar3
-    let (col, row) = (img.cols(), img.rows());
-
-    println!("channels: {}", img.channels());
-    println!("depth: {}", img.depth());
-    println!("type: {}", type_to_str(img.typ()).unwrap());
-
-    // make mask
-    // 47, 158, 68 color target
-    // filter img according to color
-
-    // BGR
-    let color = Scalar::new(75., 151., 57., 1.);
-    let mut mask = Mat::default();
-    in_range(&img, &color, &color, &mut mask)?;
-
-    // perform AND bitwise operation at img
-    // and pass the result img to "output" variable/Mat
-    let mut output = Mat::zeros(row, col, CV_8UC3)?.to_mat()?;
-
-    bitwise_and(&img, &img, &mut output, &mask)?;
-    cvt_color(&output.clone(), &mut output, COLOR_BGRA2GRAY, 0)?;
-    canny(&output.clone(), &mut output, 150., 175., 5, true)?;
-
-    // find contours
-    let mut contours: core::Vector<core::Vector<Point>> = core::Vector::default();
-    find_contours(
-        &output,
-        &mut contours,
-        RETR_EXTERNAL,
-        CHAIN_APPROX_SIMPLE,
-        Point { x: 0, y: 0 },
-    )?;
-
-    for contour in contours.iter() {
-        println!("----------------------------");
-        // get moment
-        let moment = moments(&contour, true)?;
-        let x = (moment.m10 / moment.m00) as i32;
-        let y = (moment.m01 / moment.m00) as i32;
-
-        let max_val = i32::max_value();
-        let top = contour
-            .iter()
-            .fold(Point::new(max_val, max_val), |acc, point| {
-                let acc_sum = acc.x.saturating_add(acc.y);
-                if point.x + point.y <= acc_sum {
-                    Point::new(point.x, point.y)
-                } else {
-                    acc
-                }
-            });
-
-        let min_val = i32::min_value();
-        let bot = contour
-            .iter()
-            .fold(Point::new(min_val, min_val), |acc, point| {
-                let acc_sum = acc.y.saturating_add(acc.x);
-                if point.y + point.x >= acc_sum {
-                    Point::new(point.x, point.y)
-                } else {
-                    acc
-                }
-            });
-
-        circle_def(&mut img, Point::new(x, y), 5, Scalar::new(0., 0., 255., 1.))?;
-        circle_def(
-            &mut img,
-            Point::new(top.x, top.y),
-            10,
-            Scalar::new(0., 0., 255., 1.),
-        )?;
-        circle_def(
-            &mut img,
-            Point::new(bot.x, bot.y),
-            10,
-            Scalar::new(0., 0., 255., 1.),
-        )?;
-    }
-
-    draw_contours(
-        &mut img,
-        &contours,
-        -1,
-        Scalar::new(0., 0., 255., 0.),
-        1,
-        LINE_8,
-        &no_array(),
-        1,
-        Point::new(0, 0),
-    )?;
-
-    // render image
-    while wait_key(0)? != 'q' as i32 {
-        set_mouse_callback("Image", Some(Box::new(print_coords)))?;
-        imshow("Image", &img)?;
-    }
-
-    Ok(())
 }
