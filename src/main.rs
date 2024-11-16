d9ba51da8a1b5a47daa93ac5a3c5bd50571ca7a3
Askepsa <jam.lugtus@gmail.com>
2024-11-16 15:54:55 +0800

    done pero ang bagal

--- src/main.rs
+++ src/main.rs
@@ -2,58 +2,60 @@
 use opencv::core::{
     self, count_non_zero, in_range, Mat, MatExprTraitConst, MatTrait, MatTraitConst, Scalar,
 };
-use opencv::highgui::{self, imshow, set_mouse_callback, wait_key_ex};
 use opencv::imgproc::{self, cvt_color_def};
 use rustautogui::{RustAutoGui, Screen};
 use std::error;
+use std::thread;
+use std::time::Duration;
 
 type Error = Box<dyn error::Error>;
 
-fn print_coords(event: i32, x: i32, y: i32, _: i32) {
-    if let Ok(event) = highgui::MouseEventTypes::try_from(event) {
-        match event {
-            highgui::MouseEventTypes::EVENT_LBUTTONDOWN => println!("x: {x} y: {y}"),
-            _ => (),
-        }
-    };
-}
+// fn print_coords(event: i32, x: i32, y: i32, _: i32) {
+//     if let Ok(event) = highgui::MouseEventTypes::try_from(event) {
+//         match event {
+//             highgui::MouseEventTypes::EVENT_LBUTTONDOWN => println!("x: {x} y: {y}"),
+//             _ => (),
+//         }
+//     };
+// }
 
-fn main() -> Result<(), Error> {
-    let mut auto_gui = RustAutoGui::new(true);
+// x: 440 y: 298
+// x: 1626 y: 1014
+fn screenshot() -> Result<Mat, Error> {
     let mut screen = Screen::new();
-
     let x = 881;
     let y = 387;
     let width = 1019 - 881; // 138
     let height = 587 - 387; // 200
-    let mut img = Mat::zeros(height, width, core::CV_8UC4)?.to_mat()?;
+    let mut frame = Mat::zeros(height, width, core::CV_8UC4)?.to_mat()?;
     let screen_region = screen.grab_screen_image((x, y, width as u32, height as u32));
+
     for pixel in screen_region.enumerate_pixels() {
         let x = pixel.0 as i32;
         let y = pixel.1 as i32;
         let color = pixel.2 .0;
-        *img.at_2d_mut::<core::Vec4b>(y, x)? = core::Vec4b::from_array(color);
+        *frame.at_2d_mut::<core::Vec4b>(y, x)? = core::Vec4b::from_array(color);
     }
 
-    cvt_color_def(&img.clone(), &mut img, imgproc::COLOR_BGRA2BGR)?;
-    while wait_key_ex(0)? != 'q' as i32 {
-        set_mouse_callback("img", Some(Box::new(print_coords)))?;
-        imshow("img", &img)?;
-    }
+    cvt_color_def(&frame.clone(), &mut frame, imgproc::COLOR_BGRA2BGR)?;
+    cvt_color_def(&frame.clone(), &mut frame, imgproc::COLOR_BGR2HSV)?;
 
-    Ok(())
+    Ok(frame)
 }
 
-fn process(frame: &Mat) -> Result<(), Error> {
-    if is_green(frame)? {
-        println!("green");
-    } else {
-        println!("not green");
+fn main() -> Result<(), Error> {
+    // x: 881 y: 387
+    thread::sleep(Duration::from_secs(5));
+    let auto_gui = RustAutoGui::new(true);
+    auto_gui.move_mouse_to_pos(881, 387, 0.);
+    auto_gui.left_click();
+    loop {
+        let frame = screenshot()?;
+        if is_green(&frame)? {
+            auto_gui.left_click();
+            auto_gui.left_click();
+        }
     }
-
-    imshow("Color", &frame)?;
-
-    Ok(())
 }
 
 fn is_green(frame: &Mat) -> Result<bool, Error> {
