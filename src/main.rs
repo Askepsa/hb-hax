94117090fec65904929a5dcc57605859b064fbfb
Askepsa <jam.lugtus@gmail.com>
2024-11-16 15:30:30 +0800

    ig it works now

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,44 @@
-use opencv::core::{count_non_zero, in_range, Mat, MatTraitConst, Scalar};
-use opencv::highgui::{imshow, wait_key_ex};
-use opencv::imgcodecs::{self, imread};
+// #![allow(unused)]
+use opencv::core::{
+    self, count_non_zero, in_range, Mat, MatExprTraitConst, MatTrait, MatTraitConst, Scalar,
+};
+use opencv::highgui::{self, imshow, set_mouse_callback, wait_key_ex};
 use opencv::imgproc::{self, cvt_color_def};
+use rustautogui::{RustAutoGui, Screen};
 use std::error;
 
 type Error = Box<dyn error::Error>;
 
+fn print_coords(event: i32, x: i32, y: i32, _: i32) {
+    if let Ok(event) = highgui::MouseEventTypes::try_from(event) {
+        match event {
+            highgui::MouseEventTypes::EVENT_LBUTTONDOWN => println!("x: {x} y: {y}"),
+            _ => (),
+        }
+    };
+}
+
 fn main() -> Result<(), Error> {
-    let mut _blue = imread("blue.jpeg", imgcodecs::IMREAD_COLOR)?;
-    cvt_color_def(&_blue.clone(), &mut _blue, imgproc::COLOR_BGR2HSV)?;
+    let mut auto_gui = RustAutoGui::new(true);
+    let mut screen = Screen::new();
 
-    let mut _red = imread("red.png", imgcodecs::IMREAD_COLOR)?;
-    cvt_color_def(&_red.clone(), &mut _red, imgproc::COLOR_BGR2HSV)?;
+    let x = 881;
+    let y = 387;
+    let width = 1019 - 881; // 138
+    let height = 587 - 387; // 200
+    let mut img = Mat::zeros(height, width, core::CV_8UC4)?.to_mat()?;
+    let screen_region = screen.grab_screen_image((x, y, width as u32, height as u32));
+    for pixel in screen_region.enumerate_pixels() {
+        let x = pixel.0 as i32;
+        let y = pixel.1 as i32;
+        let color = pixel.2 .0;
+        *img.at_2d_mut::<core::Vec4b>(y, x)? = core::Vec4b::from_array(color);
+    }
 
-    let mut _green = imread("green.png", imgcodecs::IMREAD_COLOR)?;
-    cvt_color_def(&_green.clone(), &mut _green, imgproc::COLOR_BGR2HSV)?;
-
+    cvt_color_def(&img.clone(), &mut img, imgproc::COLOR_BGRA2BGR)?;
     while wait_key_ex(0)? != 'q' as i32 {
-        print!("green: ");
-        process(&_green)?;
-        print!("blue: ");
-        process(&_red)?;
+        set_mouse_callback("img", Some(Box::new(print_coords)))?;
+        imshow("img", &img)?;
     }
 
     Ok(())
