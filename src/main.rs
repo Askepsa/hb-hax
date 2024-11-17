651c14cc2fe0d56f63531efc7ba3e9a25a494b2a
Askepsa <jam.lugtus@gmail.com>
2024-11-17 15:35:09 +0800

    wow ok so ung templ at frame ay dapat uc3

--- src/main.rs
+++ src/main.rs
@@ -14,61 +14,9 @@ use rustautogui::{RustAutoGui, Screen};
 type Err = Box<dyn Error>;
 
 fn main() -> Result<(), Err> {
-    // sleep(Duration::from_secs(3));
-
-    let _auto_gui = RustAutoGui::new(true);
-    let mut screen = Screen::new();
-
-    //      Left         Right
-    // top: 166, 232     1718, 238
-    // bot: 160, 1044    1692, 1040
-
-    // get frame
-    // convert auto_gui buf to opencv's buf type
-    let templ = imread("target.png", imgcodecs::IMREAD_COLOR)?;
-    let img_buf = screen.grab_screen_image((166, 232, 1718 - 166, 1040 - 232));
-    let mut frame = core::Mat::zeros(
-        img_buf.height() as i32,
-        img_buf.width() as i32,
-        core::CV_8UC4,
-    )?
-    .to_mat()?;
-
-    // println!("{:?}", frame);
-
-    for pixel in img_buf.enumerate_pixels() {
-        let x = pixel.0 as i32;
-        let y = pixel.1 as i32;
-        let color = pixel.2 .0;
-        frame
-            .at_2d_mut::<core::Vec4b>(y, x)?
-            .copy_from_slice(&color);
-    }
-
-    let (x, y) = get_templ_coords(frame, templ)?;
-    println!("{x} {y}");
-
-    // get playground coords
-    // while highgui::wait_key(0)? != 'q' as i32 {
-    //     highgui::set_mouse_callback("img", Some(Box::new(on_mouse)))?;
-    //     highgui::imshow("img", &frame)?;
-    // }
-
-    Ok(())
-}
-
-fn _on_mouse(event: i32, x: i32, y: i32, _: i32) {
-    use highgui::MouseEventTypes;
-
-    if let Ok(event) = MouseEventTypes::try_from(event) {
-        if event == MouseEventTypes::EVENT_LBUTTONUP {
-            println!("{x}, {y}");
-        }
-    }
-}
-
-fn get_templ_coords(frame: core::Mat, templ: core::Mat) -> Result<(i32, i32), Err> {
     // template matching
+    let frame = imread("playground.png", imgcodecs::IMREAD_COLOR)?;
+    let templ = imread("target.png", imgcodecs::IMREAD_COLOR)?;
     let mut templ_match = core::Mat::new_size_with_default(
         core::Size::new(
             frame.cols() - templ.cols() + 1,
@@ -97,5 +45,22 @@ fn get_templ_coords(frame: core::Mat, templ: core::Mat) -> Result<(i32, i32), Er
         &no_array(),
     )?;
 
-    Ok((max_loc.x / templ.cols(), max_loc.y / templ.cols()))
+    println!("Frame:\n{:#?}", frame);
+    println!("Templ:\n{:#?}", templ);
+    println!("Match:\n{:#?}", templ_match);
+
+    let coords = ((max_loc.x / templ.cols(), max_loc.y / templ.cols()));
+    println!("{:?}", coords);
+
+    Ok(())
+}
+
+fn _on_mouse(event: i32, x: i32, y: i32, _: i32) {
+    use highgui::MouseEventTypes;
+
+    if let Ok(event) = MouseEventTypes::try_from(event) {
+        if event == MouseEventTypes::EVENT_LBUTTONUP {
+            println!("{x}, {y}");
+        }
+    }
 }
