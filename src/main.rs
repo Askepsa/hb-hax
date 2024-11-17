b9199b584b0d7901149e38e2f84da30513ed7a32
Askepsa <jam.lugtus@gmail.com>
2024-11-17 14:26:08 +0800

    yay nababasa na ang screen lol

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-#![allow(unused)]
+// #![allow(unused)]
 
 use std::error::Error;
 
-use opencv::core::{self, no_array, MatTrait, MatTraitConst};
+use opencv::core::{self, no_array, MatExprTraitConst, MatTrait, MatTraitConst};
 use opencv::highgui;
 use opencv::imgcodecs::{self, imread};
-use opencv::imgproc::{self, match_template};
+use opencv::imgproc::{self, cvt_color, match_template};
 
 use rustautogui::{RustAutoGui, Screen};
 
@@ -15,31 +15,34 @@ fn main() -> Result<(), Err> {
     let auto_gui = RustAutoGui::new(true);
     let mut screen = Screen::new();
 
-    // Left         Right
-    // 166, 232     1718, 238
-    // 160, 1044    1692, 1040
+    //      Left         Right
+    // top: 166, 232     1718, 238
+    // bot: 160, 1044    1692, 1040
 
     // get frame
     // convert auto_gui buf to opencv's buf type
-    let img_buf = screen.grab_screen_image((166, 232, 1692, 1040));
-    let mut frame = core::Mat::new_size_with_default(
-        core::Size::new(img_buf.width() as i32, img_buf.height() as i32),
+    let img_buf = screen.grab_screen_image((166, 232, 1718 - 166, 1040 - 232));
+    let mut frame = core::Mat::zeros(
+        img_buf.height() as i32,
+        img_buf.width() as i32,
         core::CV_8UC4,
-        core::Scalar::default(),
-    )?;
+    )?
+    .to_mat()?;
+    cvt_color(&frame.clone(), &mut frame, imgproc::COLOR_RGBA2BGRA, 0)?;
+
     for pixel in img_buf.enumerate_pixels() {
         let x = pixel.0 as i32;
         let y = pixel.1 as i32;
-        let color = pixel.2.0;
+        let color = pixel.2 .0;
         frame
-            .at_2d_mut::<core::Vec4b>(x, y)?
+            .at_2d_mut::<core::Vec4b>(y, x)?
             .copy_from_slice(&color);
     }
 
     // get playground coords
     while highgui::wait_key(0)? != 'q' as i32 {
-        highgui::set_mouse_callback("img", Some(Box::new(on_mouse)));
-        highgui::imshow("img", &frame);
+        highgui::set_mouse_callback("img", Some(Box::new(on_mouse)))?;
+        highgui::imshow("img", &frame)?;
     }
 
     Ok(())
@@ -95,5 +98,5 @@ fn get_target_coords(frame: core::Mat, templ: core::Mat) -> Result<(i32, i32), E
         0,
     )?;
 
-    Ok((69, 69))
+    Ok((69, 420))
 }
